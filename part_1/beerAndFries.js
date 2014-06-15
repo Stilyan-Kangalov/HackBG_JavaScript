0829568101a21e5627186266fc0c07b35f1e4c07
Stilyan-Kangalov <stilyan_kangalov@abv.bg>
2014-06-15 11:23:00 +0300

    Files for JS exam.

---
+++ part_1/beerAndFries.js
@@ -0,0 +1,45 @@
+  "use strict";
+
+  var beerAndFries = function(n) {
+  var funcData = n;
+
+  var allBeers = [];
+  var allFries = [];
+  var firstSum = 0;
+  var secondSum = 0;
+
+  for (var i = 0; i < funcData.length; i++) {
+      if(funcData[i].type == "beer") {
+         allBeers[i] = funcData[i].score;
+      } else if (funcData[i].type == "fries") {
+       allFries[i] = funcData[i].score;
+    }
+  }
+
+  var convArray = function(data) {
+  data.filter(function(i){return true;});
+  data.sort(function(a, b){return b-a});
+  return data;
+
+  }
+
+  convArray(allBeers);
+  convArray(allFries);
+
+  if (allBeers.length == 1) {
+      firstSum = allBeers[0]*allFries[0];
+      return firstSum;
+  }  else if (allBeers.length < 1) {
+      firstSum = 0;
+      return firstSum;
+  }  else if (allBeers.length > 1) {
+      for (var i=0; i < allBeers.length; i++) {
+          secondSum += (allBeers[i] * allFries[i]);
+      }
+      return secondSum;
+  }
+
+  };
+
+
+  exports.beerAndFries = beerAndFries;
---
+++ part_2/timer.js
@@ -0,0 +1,77 @@
+"use strict";
+
+$(document).ready(function() {
+
+var firstInputMin = $("#minutes").val();
+var secondInputSec = $("#seconds").val();
+var isTimerON = false;
+var $seconds = 1000;
+
+
+var handleOne = function() {
+    setInterval(function() {
+      $("#second-first-digit").text(secondInputSec);
+      secondInputSec--;
+      $("#minute-first-digit").text(firstInputMin);
+      timeContolDown();
+      }, $seconds);
+}
+
+var handleTwo = function() {
+    setInterval(function() {
+      $("#second-first-digit").text(secondInputSec);
+      secondInputSec++;
+      $("#minute-first-digit").text(firstInputMin);
+      timeContolUp();
+      }, $seconds);
+}
+
+function timeForward() {
+    if (!isTimerON) {
+        isTimerON = true;
+        handleTwo();
+}
+}
+
+function timeBackward() {
+    if (!isTimerON) {
+        isTimerON = true;
+        handleOne();
+}
+}
+
+function timeContolDown() {
+  if (secondInputSec == -1) {
+    firstInputMin--;
+    secondInputSec = 59;
+  }
+  if (firstInputMin < 0) {
+    clearInterval();
+    $(".fixer").text(0);
+    $(".fix").text(0);
+  }
+}
+
+function timeContolUp() {
+  if (secondInputSec == 61) {
+    firstInputMin++;
+    secondInputSec = 1;
+  }
+}
+
+$("button:first").on("click", function() {
+    timeForward();
+  });
+
+$("button:nth-of-type(2)").on("click", function() {
+    timeBackward();
+  });
+
+$("button:last").on("click", function() {
+    clearInterval(handleOne);
+    clearInterval(handleTwo);
+    $(".fixer").text("00");
+    $(".fix").text("00");
+  });
+
+});
